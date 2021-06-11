2a60cdfcd357e969a9cfae83bcbdd0e33119e761
MOnk <ptvndk@gmail.com>
2021-06-11 10:18:45 +0200

    1. Activity set to landscape 2. Empty validate activity and layout added

---
+++ app/src/main/java/com/CDIOgroup18/cameraxapp/ValidateActivity.kt
@@ -0,0 +1,7 @@
+package com.CDIOgroup18.cameraxapp
+
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_validate.*
+
+class ValidateActivity : AppCompatActivity()  {
+}
