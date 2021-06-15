ec423402d3de1fb5965683f6083953ead0a11574
kasperbuurDK <kasperbuur@gmail.com>
2021-06-15 12:54:24 +0200

    kasper tuesday working on stable game ID and user experience

---
+++ app/src/main/java/com/CDIOgroup18/cameraxapp/DataStorage.kt
@@ -0,0 +1,8 @@
+package com.CDIOgroup18.cameraxapp
+
+class DataStorage {
+
+    fun DataStorage(){
+    var imageCounter = 0
+    }
+}
--- app/src/main/java/com/CDIOgroup18/cameraxapp/RestartToServerClass.kt
+++ app/src/main/java/com/CDIOgroup18/cameraxapp/RestartToServerClass.kt
@@ -4,23 +4,20 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
 
-class RestartToSetverClass :Thread() {
+class RestartToServerClass :Thread() {
 
     private val client = OkHttpClient()
 
     override fun run() {
         val request = Request.Builder()
-            .url("http://130.225.170.93:9001/api/v1/restart")
+            .url("http://130.225.170.93:9001/api/v1/restart/${MainActivity.myGameID}")
             .build()
 
         client.newCall(request).execute().use { response ->
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-            for ((name, value) in response.headers) {
-                println("$name: $value")
-            }
-
-            println("hey"+response.body!!.string())
         }
+
+
     }
 }
